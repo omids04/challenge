d4da9c454ed086b8e91a584f1e868c68f3cbec82
omid <omid.sedighi04@gmail.com>
2022-01-23 13:02:44 +0330

    project setUp

---
+++ src/main/java/com/dimo/challenge/App.java
@@ -0,0 +1,5 @@
+package com.dimo.challenge;
+
+public class App {
+
+}
---
+++ src/test/java/com/dimo/challenge/AppTest.java
@@ -0,0 +1,13 @@
+package com.dimo.challenge;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void test(){
+        System.out.println("All is ok");
+    }
+}
